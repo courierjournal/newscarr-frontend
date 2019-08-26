e3354d342e2ad0c0a19d3f37914550449246702d
Jesse Hazel <jwhazel@me.com>
2019-08-26 14:31:20 -0400

    Cleanup baseUrl

--- src/assets/libs/baseUrl.js
+++ src/assets/libs/baseUrl.js
@@ -1,4 +1,5 @@
 /**
  * Used to define the base path used to access the backend
  */
-export const baseUrl = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8000";
+export const baseUrl =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8000";
